b274e880c2d964261a21a09af39098b58dc7bc03
Guillermo Campelo <guicamest@gmail.com>
2015-07-26 13:26:19 -0300

    Added jodatime module

---
+++ jodatime/src/main/java/com/sleepcamel/bsoneer/processor/codeprovider/JodaTimeDecoder.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2015 Sleepcamel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.sleepcamel.bsoneer.processor.codeprovider;
+
+import javax.lang.model.type.TypeMirror;
+
+import org.joda.time.DateTime;
+import org.kohsuke.MetaInfServices;
+
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.CodeBlock;
+
+@MetaInfServices(value=DecodeCodeProvider.class)
+public class JodaTimeDecoder  extends SingleStatementDecodeCodeProvider {
+
+	@Override
+	public boolean applies(TypeMirror tm) {
+		return DateTime.class.getCanonicalName().equals(tm.toString());
+	}
+	
+	@Override
+	public void putDecodeCode(CodeBlock.Builder cb, TypeMirror tm, DecodeCodeProviders decodeProviders, String varname, boolean declareVariable) {
+		cb.add("new $T(reader.readDateTime())", ClassName.get(DateTime.class));
+	}
+}
